159b2a7cf57487bbbc5ed74c7856d4cd15caa6df
mlopera01 <miramaandrea@gmail.com>
2022-11-29 08:00:09 -0500

    Subo casos de pruebas

--- src/test/java/com/example/crudCliente/CrudClienteApplicationTests.java
+++ src/test/java/com/example/crudCliente/CrudClienteApplicationTests.java
@@ -1,23 +1,13 @@
 package com.example.crudCliente;
-
 import com.example.crudCliente.controller.Client;
 import com.example.crudCliente.service.ClientService;
 import com.example.crudCliente.service.impl.ClientServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Date;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.springframework.test.util.AssertionErrors.assertEquals;
-
-//@SpringBootTest
+//@SpringBootTest usamos Jupiter, esto no aplica
 class CrudClienteApplicationTests {
 	//@Autowired
 	private ClientService clienteService = new ClientServiceImpl();
-
 	@Test
 	void testCreateClient_WhenDocumentNotNull_ThenCreated() {
 		Client client = new Client();
@@ -25,20 +15,20 @@ class CrudClienteApplicationTests {
 		client.setFirstName("Ingrid");
 		client.setLastName("Hernandez");
 		client.setDocument("43110225");
-		client.setCreatedDate(new Date());
 		client.setEmail("ihernand@proteccion.com.co");
-		//assertThat(client.getCreatedDate()).isNotNull();
-		System.out.println("Cliente: "+client);
-		Assertions.assertEquals("43110225",client.getDocument());
+		Client result = clienteService.createClient(client); //aca invocamo la funcion crear
+		Assertions.assertNotNull(result.getCreatedDate()); //como usamos Jupiter no requiere mensaje
+		System.out.println("Client: "+ result);
+		Assertions.assertEquals(client.getDocument(),result.getDocument()); //valida si fue seteado el cliente retornado
+		Assertions.assertEquals(client.getFirstName(),result.getFirstName());
 	}
 	@Test
 	void testUpdateClient_WhenDocumentNotNull_ThenRegistrationDateNotNull() {
 		Client client = new Client();
 		client.setDocument("43630233");
-		Client clientResult = clienteService.updateClient("123", client);
-		//client.setCreatedDate(null);
-		Assertions.assertNotNull(clientResult.getUpdatedDate());
-		System.out.println("Cliente Actualizado: "+clientResult);
+		Client result = clienteService.updateClient("123", client);
+		Assertions.assertNotNull(result.getUpdatedDate());
+		System.out.println("Update Client: "+ result);
 		//Assertions.assertEquals("71790754",client.getDocument());
 	}
 
@@ -50,7 +40,6 @@ class CrudClienteApplicationTests {
 		client.setPhone("3013325522");
 		Assertions.assertThrows(IllegalArgumentException.class,
 				() -> clienteService.updateClient("71790754", client));
-
 		//Client client1 = ;
 		//Assertions.assertEquals("71790754",client1.getDocument());
 		//Assertions.assertNotEquals("43630233",client1.getDocument());
